30d1532f4a004ede1a76c1b62288084d14eba029
nopecho <ch.jooon@gmail.com>
2022-10-10 01:35:27 +0900

    test

--- HELP.md
+++ HELP.md
@@ -21,3 +21,29 @@ These additional references should also help you:
 
 * [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
 
+문제
+- MAC M1 docker 환경에서 oracle 실행 불가
+
+원인
+- oracle 자체에서 MAC M1 arm 아키텍쳐를 지원하지 않음
+- M1에 설치된 docker도 arm 아키텍쳐 기반 이미지 사용으로 오라클 지원 x
+
+해결
+- arm 아키텍쳐를 지원하는 docker runtime 설치 후 도커 이미지 실행
+
+1. colima 설치 
+brew install colima
+
+2. colima 실행
+colima start --arch x86_64 --memory 4
+
+2-1. docker runtime 확인
+docker context ls
+
+3. docker run
+docker run -p 8080:8080 -p 1521:1521 jaspeen/oracle-xe-11g
+
+4. SQLDeploper 실행
+ID : system
+PW : oracle
+
--- batch/src/main/java/com/nopecho/job/JobConfiguration.java
+++ batch/src/main/java/com/nopecho/job/JobConfiguration.java
@@ -8,14 +8,15 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.batch.api.chunk.ItemProcessor;
-import javax.batch.api.chunk.ItemReader;
 
 @Slf4j
 @Configuration
@@ -35,16 +36,18 @@ public class JobConfiguration {
     }
 
     @Bean
+    @JobScope
     public Step firstStep() {
         return stepBuilderFactory.get("firstStep")
-                .chunk(1000)
+                .<Item, Item>chunk(1000)
                 .reader(itemReader())
                 .processor(itemProcessor())
-                .writer()
+                .writer(itemWriter())
                 .build();
     }
 
     @Bean
+    @StepScope
     public JdbcCursorItemReader<Item> itemReader() {
         return new JdbcCursorItemReaderBuilder<Item>()
                 .sql("SELECT * FROM ITEM")
@@ -52,7 +55,18 @@ public class JobConfiguration {
     }
 
     @Bean
+    @StepScope
     public ItemProcessor<Item, Item> itemProcessor() {
+        return item -> {
+            item.changeToken();
+            return item;
+        };
+    }
+
+    @Bean
+    @StepScope
+    public JdbcBatchItemWriter<Item> itemWriter() {
+        return new JdbcBatchItemWriter<>();
     }
 
     @Bean
